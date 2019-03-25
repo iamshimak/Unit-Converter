c98af71459aa9111925db9b71b6b478450ab8a84
Ahamed Shimak <>
2019-03-26 02:43:56 +0530

    Initial Commit

---
+++ UnitConverter/Views/BrandTextField.swift
@@ -0,0 +1,33 @@
+//
+//  BrandTextField.swift
+//  UnitConverter
+//
+//  Created by Suwadith Srithar on 3/26/19.
+//  Copyright © 2019 Ahamed Shimak. All rights reserved.
+//
+
+import UIKit
+
+class BrandTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setup() {
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
