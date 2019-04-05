9bb08cfd0978657072103794f3a56280b9122175
Shimak Ahamed <shimak2013@gmail.com>
2019-04-05 15:22:00 +0530

    TextFields Setup

--- UnitConverter/Views/EquationTextField.swift
+++ UnitConverter/Views/EquationTextField.swift
@@ -1,25 +1,27 @@
 //
-//  BrandTextField.swift
+//  EquationTextField.swift
 //  UnitConverter
 //
-//  Created by Suwadith Srithar on 3/26/19.
+//  Created by student on 4/5/19.
 //  Copyright © 2019 Ahamed Shimak. All rights reserved.
 //
 
 import UIKit
 
-class BrandTextField: UITextField {
-    
+class EquationTextField: UITextField {
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setup()
     }
     
     func setup() {
-        
+        textAlignment = .right
     }
 
 }
